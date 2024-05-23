a54df091ce95c19115c23c8f7e5d54ba6cd248c4
Parin Parikh <59714790+parin-parikh@users.noreply.github.com>
2024-05-23 21:18:00 +0000

    Cleaned up comments

--- package-dotnet/Program.cs
+++ package-dotnet/Program.cs
@@ -10,9 +10,6 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        // Define your Azure subscription ID
-        //string subscriptionId = "4ea324a6-6d16-4156-98a8-3485ecaf0a1e";
-
         // Define the resource group and storage account names
         string resourceGroupName = "my-sdk-rg";
         string location = "westus";
@@ -23,15 +20,10 @@ class Program
 
         // Get the subscription object
         SubscriptionResource subscription = await client.GetDefaultSubscriptionAsync();
-        /*  SubscriptionResource subscription = await client.GetSubscriptionAsync(); */
-        /* resourceGroupCollection resourceGroup = subscription.GetResourceGroups();  */
         ResourceGroupCollection resourceGroups = subscription.GetResourceGroups();
 
         // Create a resource group
         ResourceGroupData resourceGroupData = new ResourceGroupData(location);
-
-        /* resourceGroupData resourceGroupData = new ResourceGroupData(location); */
         ArmOperation<ResourceGroupResource> operation = await resourceGroups.CreateOrUpdateAsync(WaitUntil.Completed, resourceGroupName, resourceGroupData);
-        /* ResourceGroupResource resourceGroup = operation.Value; */
     }
 }
